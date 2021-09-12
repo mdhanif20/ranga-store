a4b142f0f5917ea11812411ea2878f3a9ffa958c
mdhainf20 <mdhanif115825@gmail.com>
2021-09-12 23:53:17 +0600

    price and total bug fixed

--- js/app.js
+++ js/app.js
@@ -31,10 +31,12 @@ const showProducts = (products) => {
 };
 let count = 0;
 const addToCart = (id, price) => {
+  console.log(id,price);
   count = count + 1;
   updatePrice("price", price);
-
   updateTaxAndCharge();
+  // update total bug fixed 
+  updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
 
@@ -49,12 +51,12 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = /* Math.round */ parseFloat(total).toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = /* Math.round */ value;
 };
 
 // update delivery charge and total Tax
@@ -62,7 +64,7 @@ const updateTaxAndCharge = () => {
   const priceConverted = getInputValue("price");
   if (priceConverted > 200) {
     setInnerText("delivery-charge", 30);
-    setInnerText("total-tax", priceConverted * 0.2);
+    setInnerText("total-tax", priceConverted * 0.2 );
   }
   if (priceConverted > 400) {
     setInnerText("delivery-charge", 50);
